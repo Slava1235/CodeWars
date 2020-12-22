1be479703ac11561e3c8da565d887f4652d34030
Ваше Имя <you@example.com>
2020-12-22 22:35:06 +0500

    Greate first project

---
+++ src/Test.java
@@ -0,0 +1,25 @@
+
+import java.util.Arrays;
+
+
+public class Test {
+
+    public static void main(String[] args) {
+        String test = "One";
+        String original = "neo";
+
+        System.out.println(isAnagram(test, original));
+    }
+
+    public static boolean isAnagram(String test, String original) {
+        // your code goes here
+        char[] word1 = test.toLowerCase().replaceAll("[\\s]", " ").toCharArray();
+        char[] word2 = original.toLowerCase().replaceAll("[\\s]", " ").toCharArray();
+        Arrays.sort(word1);
+        Arrays.sort(word2);
+        if (Arrays.equals(word1, word2)) {
+            return true;
+        }
+        return false;
+    }
+}
