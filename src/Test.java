1b2e69c7be38719107f6de901125a493aff9f529
Slava1235 <starslava14@gmail.com>
2020-12-24 22:55:33 +0500

    Update first Test

--- src/Test.java
+++ src/Test.java
@@ -7,20 +7,18 @@ public class Test {
     public static void main(String[] args) {
         String test = "One";
         String original = "neo";
-
         System.out.println(isAnagram(test, original));
     }
 
     public static boolean isAnagram(String test, String original) {
         // your code goes here
-        char[] word1 = test.toLowerCase().replaceAll("[\\s]", " ").toCharArray();
-        char[] word2 = original.toLowerCase().replaceAll("[\\s]", " ").toCharArray();
+        char[] word = test.toLowerCase().replaceAll("[\\s]", " ").toCharArray();
+        char[] word1 = original.toLowerCase().replaceAll("[\\s]", " ").toCharArray();
+        Arrays.sort(word);
         Arrays.sort(word1);
-        Arrays.sort(word2);
-        if (Arrays.equals(word1, word2)) {
+        if (Arrays.equals(word, word1)) {
             return true;
         }
-
         return false;
     }
 }
