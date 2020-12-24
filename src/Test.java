644bc2ed225c6112ae02416f8c455c82852320d1
Slava1235 <starslava14@gmail.com>
2020-12-24 23:19:43 +0500

    three branch

--- src/Test.java
+++ src/Test.java
@@ -3,24 +3,12 @@ import java.util.Arrays;
 
 
 public class Test {
-    static String test = "One";
-    static String original = "neo";
 
     public static void main(String[] args) {
-
+        String test = "One";
+        String original = "neo";
         System.out.println(isAnagram(test, original));
     }
-
-
-
-
-
-
-
-
-
-
-
 
     public static boolean isAnagram(String test, String original) {
         // your code goes here
